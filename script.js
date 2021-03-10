de8988c478d0095ff04f7f1ec5e230f9d2e46c1e
Artem-grif <artem.osmak@ukr.net>
2021-03-10 21:27:08 +0200

    13 Урок - Условия

--- script.js
+++ script.js
@@ -1,26 +1,36 @@
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+// if (4) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!');
+// }
 
+// const num = 50;
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Too much!');
+// } else {
+//     console.log('Ok!');
+// }
 
-};
+// (num === 50) ? console.log('Ok!') : console.log('Error!');
 
+const num = '50';
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
- 
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-
-console.log(personalMovieDB);
+switch (num) {
+    case '49':
+        console.log('Incorrectly!');
+        break;
+    case '100':
+        console.log('Incorrectly!');
+        break;
+    case '50':
+        console.log('Correctly!');
+        break;
+    default:
+        console.log('Not this time!');
+        break;
+}
