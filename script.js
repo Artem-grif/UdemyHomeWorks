73ffe9f9df7e53bbed0d785b5ba931824c9c4997
Artem-grif <artem.osmak@ukr.net>
2021-03-10 23:00:53 +0200

    Добавил из коммита с первой практической работой все данные в последний урок

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ for (let i = 1; i < 10; i++) {
     console.log(i);
 
 }
+
+
+
+
+const numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+
+};
+
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+ 
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+
+console.log(personalMovieDB);
