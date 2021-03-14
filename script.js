9f311ef983349a79115bfa1cfdabb79f5c0fd26c
Artem-grif <artem.osmak@ukr.net>
2021-03-14 19:25:23 +0200

    Урок 15. Выполнение второго практического задания с циклами условиями

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let num = 50;
+// let num = 50;
 
 // while (num <= 55) {
 //     console.log(num);
@@ -14,15 +14,15 @@ let num = 50;
 // }
 // while (num <= 55);
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }
 
-    console.log(i);
+//     console.log(i);
 
-}
+// }
 
 
 
@@ -40,14 +40,35 @@ const personalMovieDB = {
 };
 
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
- 
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из последних просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+for (let i = 0; i <= 1; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if (a == null || a.length > 50 || a == '' || b == null || b.length > 50 || b == '') {
+        console.log('Error!');
+        i--;
+    } else {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    }
+
+}
+
+if (numberOfFilms < 10 && numberOfFilms > 0) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms <= 30){
+    alert('Вы классический зритель');
+} else if (numberOfFilms > 30) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка');
+}
 
 
 console.log(personalMovieDB);
