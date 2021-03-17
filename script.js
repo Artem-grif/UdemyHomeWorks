204f814a864e1265810042ca71590628fbc2d393
Artem-grif <artem.osmak@ukr.net>
2021-03-17 21:30:35 +0200

    Lesson 18. Практика , ч3. Используем функции

--- script.js
+++ script.js
@@ -1,31 +1,95 @@
 'use strict';
 
-//Lesson 17. Методы и свойства строк и чисел
-
-const str = 'teSt';
+//Lesson 18. Практика , ч3. Используем функции
 
 
-// console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+let numberOfFilms;
 
-const fruit = 'some fruit';
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
 
-console.log(fruit.indexOf('q'));
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
+    }
+}
 
-
-const logg = 'Hello world!';
-
-// console.log(logg.slice(6, 12));
-
-// console.log(logg.substring(12, 6));
-
-console.log(logg.substr(6, 6));
+// start();
 
 
-const num = 12.2;
-console.log(Math.round(num));
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 
-const test = '12.2px';
-// console.log(parseInt(test));
-console.log(parseFloat(test));
+};
+
+
+function rememberMyFilms() {
+    for (let i = 0; i <= 1; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
+
+        if (a == null || a.length > 50 || a == '' || b == null || b.length > 50 || b == '' || isNaN(b)) {
+            console.log('Error!');
+            i--;
+        } else {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        }
+    }
+}
+
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (numberOfFilms < 10 && numberOfFilms > 0) {
+        alert('Просмотрено довольно мало фильмов');
+    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
+        alert('Вы классический зритель');
+    } else if (numberOfFilms > 30) {
+        alert('Вы киноман');
+    } else {
+        alert('Произошла ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+
+function showMyDB() { //Первый способ
+    if (personalMovieDB.privat == false) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
+
+// function showMyDB(hidden) {                        //Второй способ
+//     if (!hidden) {
+//         console.log(personalMovieDB);
+//     }
+// }
+
+// showMyDB(personalMovieDB.privat);
+
+
+// function writeYourGenres() {                    // Первый способ
+//     for (let i = 1; i <= 3; i++) {
+//         const a = prompt(`Ваш любимый жанр под номером ${i}?`, '');
+//         personalMovieDB.genres[i - 1] = a;
+//     }
+// }
+
+// writeYourGenres();
+
+
+function writeYourGenres() { // Второй способ
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');       // Вставляем вопрос вместо промежуточной переменной.
+    }
+}
+
+writeYourGenres();
