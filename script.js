f640f1624301cccf046c463f704834d1da3cf55e
Artem-grif <artem.osmak@ukr.net>
2021-03-18 21:08:57 +0200

    Lesson 18. Callback - функции

--- script.js
+++ script.js
@@ -1,95 +1,28 @@
 'use strict';
 
-//Lesson 18. Практика , ч3. Используем функции
+//Lesson 18. Callback - функции
 
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-
-    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов Вы уже посмотрели?', '');
-    }
+function first() {
+    //Do something
+    setTimeout(function() {
+        console.log(1);
+    }, 500);
 }
 
-// start();
-
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-
-};
-
-
-function rememberMyFilms() {
-    for (let i = 0; i <= 1; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ''),
-            b = prompt('На сколько оцените его?', '');
-
-        if (a == null || a.length > 50 || a == '' || b == null || b.length > 50 || b == '' || isNaN(b)) {
-            console.log('Error!');
-            i--;
-        } else {
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        }
-    }
+function second() {
+    console.log(2);
 }
 
-// rememberMyFilms();
+first();
+second();
 
-
-function detectPersonalLevel() {
-    if (numberOfFilms < 10 && numberOfFilms > 0) {
-        alert('Просмотрено довольно мало фильмов');
-    } else if (numberOfFilms >= 10 && numberOfFilms <= 30) {
-        alert('Вы классический зритель');
-    } else if (numberOfFilms > 30) {
-        alert('Вы киноман');
-    } else {
-        alert('Произошла ошибка');
-    }
+function learnJS(lang, callback) {
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-// detectPersonalLevel();
-
-
-function showMyDB() { //Первый способ
-    if (personalMovieDB.privat == false) {
-        console.log(personalMovieDB);
-    }
+function done () {
+    console.log("Я прошел этот урок!");
 }
 
-showMyDB();
-
-// function showMyDB(hidden) {                        //Второй способ
-//     if (!hidden) {
-//         console.log(personalMovieDB);
-//     }
-// }
-
-// showMyDB(personalMovieDB.privat);
-
-
-// function writeYourGenres() {                    // Первый способ
-//     for (let i = 1; i <= 3; i++) {
-//         const a = prompt(`Ваш любимый жанр под номером ${i}?`, '');
-//         personalMovieDB.genres[i - 1] = a;
-//     }
-// }
-
-// writeYourGenres();
-
-
-function writeYourGenres() { // Второй способ
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}?`, '');       // Вставляем вопрос вместо промежуточной переменной.
-    }
-}
-
-writeYourGenres();
+learnJS('JavaScript', done);
